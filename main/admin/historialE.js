9792a75b7556965d9a8b815a124e2674011caebc
APereiraScripts <pereirapuelloandresdavid@gmail.com>
2024-09-12 18:20:32 -0500

    falta el formulario

--- main/admin/historialE.js
+++ main/admin/historialE.js
@@ -13,4 +13,24 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = 'none';
     }
+};
+var model = document.getElementById("myModal");
+var boton = document.getElementById("openModalBtn");
+var cerrar = document.getElementsByClassName("close")[0];
+
+
+boton.onclick = function() {
+    model.style.display = "block";
 }
+
+
+cerrar.onclick = function() {
+    model.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == model) {
+        model.style.display = "none";
+    }
+};
