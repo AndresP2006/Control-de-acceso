37188d084c1132f0e01a18c86b6ac0952af4c170
juancm06 <jcharryme1@gmail.com>
2024-11-09 22:14:37 -0500

    Add module login and estructure database

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -3,14 +3,31 @@
 class LoginController extends Controlador
 {
 
+    private $UserModel;
+
     public function __construct()
     {
+        $this->UserModel = $this->modelo('UserModel');
         //echo 'Controlador paginas cargado';
     }
 
     public function index()
     {
-        $this->vista('pages/admin/adminView', null);
+        $result = $this->UserModel->getUserByEmailOrName($_POST['usuario']);
+
+        if (isset($result)) {
+            if ($result->Us_contrasena === $_POST['password']) {
+                switch ($result->Ro_id) {
+                    case "1": // administrador
+                        $this->vista('pages/admin/adminView', $result);
+                        break;
+                    case "2": // guardia
+                        $this->vista('pages/porter/porterView', $result);
+                        break;
+                }
+            }
+        }
+        // $this->vista('pages/admin/adminView', null);
     }
 
     public function verPorter()
