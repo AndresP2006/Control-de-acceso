1d1446823beb594fa94144f5ab4b82a67b8cc759
juancm06 <jcharryme1@gmail.com>
2024-11-24 20:48:23 -0500

    Cerrrar modal

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controlador
     public function index()
     {
         // $articles = $this->articleModel->getArticles();
-
+        // session_destroy();
         $datos = [
             'titulo' => 'Bienvenido a MVC render2web',
             // 'articles' => $articles
@@ -29,27 +29,30 @@ class HomeController extends Controlador
     }
 
     // desplazamiento de vistas
-    public function informacion(){
+    public function informacion()
+    {
         $this->vista('pages/home/informacionView');
     }
-    public function nosotros(){
+    public function nosotros()
+    {
         $this->vista('pages/home/nosotrosView');
     }
 
-    public function admin() {
+    public function admin()
+    {
         if (!isset($_SESSION['sesion_activa'])) {
             header('location:' . RUTA_URL . '/pages/homeView');
             exit;
         }
-    
+
         $datos = $this->userController->MostrarDatos();
         $Torres = $this->TorreModel->setTorres();
 
-        $_SESSION['torre'] = $Torres;    
+        $_SESSION['torre'] = $Torres;
         // Pasamos los datos correctamente a la vista
         $this->vista('pages/admin/adminView', $datos);
     }
-    
+
 
     public function guard()
     {
@@ -61,13 +64,16 @@ class HomeController extends Controlador
         $this->vista('pages/porter/porterView', ($this->porterController->index()));
     }
     // menu de administracion 
-    public function usuario(){
+    public function usuario()
+    {
         $this->vista('pages/admin/adminView');
     }
-    public function HistoryRecords(){
+    public function HistoryRecords()
+    {
         $this->vista('pages/admin/historialReView');
     }
-    public function HistoryPackages(){
+    public function HistoryPackages()
+    {
         $this->vista('pages/admin/paquetesView');
     }
 
