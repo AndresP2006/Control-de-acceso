a7a7e7aee8a7b608b91e51c131eb7af8a3c038a0
juancm06 <jcharryme1@gmail.com>
2024-11-26 21:17:49 -0500

    Alertas de admin

--- app/views/inc/footer-admin.php
+++ app/views/inc/footer-admin.php
@@ -1,4 +1,5 @@
 <script src="<?php echo RUTA_URL; ?>/js/usuarios.js"></script>
+<script src="<?php echo RUTA_URL; ?>/js/alert.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="<?php echo RUTA_URL; ?>/js/usuarios.js"></script>
 
