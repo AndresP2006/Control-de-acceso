ae31b145fe0890bc317dd742d97448a1d0433bac
Isaías 00123 <isaiaspinzon1996@gmail.com>
2025-04-10 07:58:47 -0500

    nuevo diseño

--- app/views/inc/header-notificacion.php
+++ app/views/inc/header-notificacion.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NOTIFICACIONES</title>
-    <link rel="stylesheet" href="<?php echo RUTA_URL?>/css/notificacion.css">
+    <link rel="stylesheet" href="<?php echo RUTA_URL ?>/css/notificacion.css">
+
 </head>
+
 <body>
-    
-</body>
-</html>
+    <div class="content">
+        <div class="encabezado">
+            <div class="titulo">
+                <h1 class="titulo_1">Control De Acceso </h1>
+            </div>
+
+            <h3 class="var_sesion">Bienvenido <?= $_SESSION['datos']->Us_usuario ?></h3>
+            <div class="cerrar-sescion">
+                <a href="<?php echo RUTA_URL; ?>/HomeController/index"><button class="boton">Cerrar Sesión</button></a>
+            </div>
+        </div>
